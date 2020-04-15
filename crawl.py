9798f5dc1f56ce31d8ef2fb5f76033a00f5ce73c
codeep2 <prothinkingl@gmail.com>
2020-04-15 18:25:05 +0800

    Update crawl.py
    Add muticorotines function

--- crawl.py
+++ crawl.py
@@ -1,5 +1,4 @@
 import asyncio
-import time
 from pyppeteer import launch
 from json import dumps
 import tldextract
@@ -8,10 +7,11 @@ class spider():
     def __init__(self, depth, ):
         self.depth = depth  # 爬取的深度,深度为0表示只爬取当前页
         self.crawl_list = []  # 待爬取列表
-        self.crawled_list = set([])  # 已经抓取的url
+        self.crawled_list = set({})  # 已经抓取的url
         self.arrange_link_url = {}  # 获取到的url
         self.arrange_load_url = {}  # 请求资源url
         self.depth_dict = {}  # 深度字典
+        self.home_page = 0 #用于首页登录测试
 
     async def visit_page(self, page, domain):
         '''
@@ -19,8 +19,15 @@ class spider():
         '''
 
         await page.goto(domain, {"waitUntil": "networkidle2"})
-        #等待1s
-        await page.waitFor(1000)
+        print('visit page:', domain)
+        print('crawl_list:', len(self.crawl_list))
+
+        #访问首页时等待30秒
+        if self.home_page < 1:
+            await page.waitFor(30000)
+            self.home_page += 1
+        else:
+            await page.waitFor(4000)
         # await page.waitForNavigation()
 
         if self.depth_dict[domain] < self.depth + 1:
@@ -28,10 +35,9 @@ class spider():
             link_list = await page.xpath('//a')
             for link in link_list:
                 url = await (await link.getProperty('href')).jsonValue()
-                if url not in self.depth_dict:
-                    self.add_crawurl(domain, url)
+                await self.add_crawurl(domain, url)
 
-    def add_crawurl(self, link, url):
+    async def add_crawurl(self, link, url):
         '''
         判断链接是否抓取过和是否合法，将合法链接加入到待爬列表里
         '''
@@ -47,7 +53,11 @@ class spider():
                 self.arrange_link_url[domain][subdomain] = []
 
             self.crawl_list.append(url)
-            self.depth_dict[url] = self.depth_dict[link] + 1
+            lock = asyncio.Lock()
+
+            async with lock:
+                self.depth_dict[url] = self.depth_dict[link] + 1
+
             self.arrange_link_url[domain][subdomain].append(url)
         else:
             return
@@ -57,21 +67,18 @@ class spider():
             return False
         elif 'http' not in url:
             return False
+        elif 'download' and '.exe' in url:
+            return False
         elif config['allow_domain'] in url:
             return True
         elif url in self.crawled_list:
             return False
 
     async def get_crawurl(self):
-        index = 1
         while True:
             if (len(self.crawl_list) == 0) :
-                time.sleep(5)
-                index += 1
+                await asyncio.sleep(5)
                 continue
-            elif index >= 3:
-                print('All url have crawled')
-                return None
 
             # 从待爬取列表中移除并获得首元素链接
             url = self.crawl_list.pop(0)
@@ -92,7 +99,6 @@ class spider():
         if subdomain not in self.arrange_load_url[domain]:
             self.arrange_load_url[domain][subdomain] = []
         #if url.endswith('js'):
-
         self.arrange_load_url[domain][subdomain].append(url)
 
     async def inject_request(self, req):
@@ -107,43 +113,41 @@ class spider():
         obj = {
             'arrange_link_url': self.arrange_link_url,
             'arrange_load_url': self.arrange_load_url,
+            #'crawl_list': self.crawled_list
             'dict_depth': self.depth_dict
         }
+        print(len(self.crawled_list))
         jsoncontent = dumps(obj, indent=4, ensure_ascii=False)
         with open(filename, 'w', encoding='utf-8') as f:
             f.write(jsoncontent)
 
 config = {
-    'start_url' : 'https://www.iqiyi.com',
-    'allow_domain': 'iqiyi.com',
-    'depth': 5,
+    'start_url': 'https://www.mgtv.com/',
+    'allow_domain': 'mgtv.com',
+    'depth': 0,
     'coroutines': 5
 }
 
+async def main():
+    task = spider(config['depth'])
+    task.depth_dict[config['start_url']] = 0
 
-async def brw():
     browser = await launch({
         'headless': False,
         'args': ['--autoplay-policy=AutoplayAllowed'],
         'ignoreHTTPSErrors': True,
         'dumpio': True,
-
+        'executablePath': 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'
     })
-    return browser
-
-async def main():
-    task = spider(config['depth'])
-    task.depth_dict[config['start_url']] = 0
-    browser = await brw()
 
     async def worker(browser, task):
         page = await browser.newPage()
         await page.setViewport(viewport={'width': 1280, 'height': 800})
-        #await page.setUserAgent(random.choice(task.user_agents))
+        await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/'
+                                '537.36 (KHTML, like Gecko) Chrome/81.0.4044.92 Safari/537.36')
         await page.setRequestInterception(True)
 
         page.on('request', task.inject_request)
-        # page.on('error', '''()=>{}''')
         await task.visit_page(page, config['start_url'])
 
         while await task.get_crawurl():
@@ -155,22 +159,21 @@ async def main():
 
     async def saver():
         while True:
-            await asyncio.sleep(10)
+            await asyncio.sleep(30)
             print('write your hell')
             task.save_tofile('output.json')
 
     #await asyncio.wait({asyncio.create_task(worker(browser, task))})
 
     task_list = [asyncio.create_task(worker(browser, task)) for _ in range(config['coroutines'])]
-    #task_list = [asyncio.create_task(worker(browser, task)), asyncio.create_task(worker(browser, task))]
+    #task_list = [asyncio.create_task(worker(browser, task))]
     task_list.append(asyncio.create_task(saver()))
 
     await asyncio.wait(task_list)
+
     #loop = asyncio.get_event_loop()
     #loop.run_until_complete(asyncio.wait(tasks))
     print("finish")
 
 if __name__ == '__main__':
     asyncio.get_event_loop().run_until_complete(main())
-
-    #main()
